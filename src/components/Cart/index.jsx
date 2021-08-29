d8a4fddbb23eec6ed129080c508e54ae7fe3e558
thiago <thiagosilva128@gmail.com>
2021-08-29 20:13:53 -0300

    added link to access checkout route through component cart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,14 +3,18 @@ import { ReactComponent as ShoppingCart } from './shopping-cart.svg'
 import { useContext } from 'react'
 import { CartContext } from '../../context/CartContext'
 
+import { Link } from "react-router-dom";
+
 export function Cart() {
     const { cart } = useContext(CartContext)
     const quantity = cart.length || 0
 
     return (
         <ContainerCart>
-            <ShoppingCart />
-            <div>{ quantity }</div>
+            <Link to="/checkout">
+                <ShoppingCart />
+                <div>{ quantity }</div>
+            </Link>
         </ContainerCart>
     )
 }
